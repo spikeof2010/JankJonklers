93ca25842896bc445d53a612362b5ebfc94589e0
MyseIf <helluser0@gmail.com>
2024-08-04 23:18:59 +0700

    1.0.0 port

--- JankChallenges.lua
+++ JankChallenges.lua
@@ -1,142 +1,107 @@
 --- STEAMODDED HEADER
 --- MOD_NAME: Jank Challenges
 --- MOD_ID: JankChallenges
+--- PREFIX: jankch
 --- MOD_AUTHOR: [Lyman]
 --- MOD_DESCRIPTION: Challenges for Jank Jonklers.
 
 ----------------------------------------------
 ------------MOD CODE -------------------------
 
+SMODS.Challenge({
+    key = "pawnshop",
+    rules = {
+        custom = {
+            {id = 'no_reward_specific', value = 'Small'},
+            {id = 'no_reward_specific', value = 'Big'},
+            {id = 'no_extra_hand_money'},
+            {id = 'no_interest'},
+        },
+    },
+    jokers = {
+        {id = 'j_jank_pawn', edition = 'negative', eternal = true},
+        {id = 'j_jank_scrapper'},
+    },
+    vouchers = {
+        {id = 'v_overstock_norm'},
+    },
+    deck = {
+        type = 'Challenge Deck',
+        cards = {{s='D',r='2',},{s='D',r='3',},{s='D',r='4',},{s='D',r='5',},{s='D',r='6',},{s='D',r='7',},{s='D',r='8',},{s='D',r='9',},{s='D',r='T',},{s='D',r='J',},{s='D',r='Q',},{s='D',r='K',},{s='D',r='A',g='Blue',},{s='C',r='2',},{s='C',r='3',},{s='C',r='4',},{s='C',r='5',},{s='C',r='6',},{s='C',r='7',},{s='C',r='8',},{s='C',r='9',},{s='C',r='T',},{s='C',r='J',},{s='C',r='Q',},{s='C',r='K',},{s='C',r='A',g='Blue',},{s='H',r='2',},{s='H',r='3',},{s='H',r='4',},{s='H',r='5',},{s='H',r='6',},{s='H',r='7',},{s='H',r='8',},{s='H',r='9',},{s='H',r='T',},{s='H',r='J',},{s='H',r='Q',},{s='H',r='K',},{s='H',r='A',g='Blue',},{s='S',r='2',},{s='S',r='3',},{s='S',r='4',},{s='S',r='5',},{s='S',r='6',},{s='S',r='7',},{s='S',r='8',},{s='S',r='9',},{s='S',r='T',},{s='S',r='J',},{s='S',r='Q',},{s='S',r='K',},{s='S',r='A',g='Blue',},}
+    },
+    restrictions = {
+        banned_cards = {
+            {id = 'c_temperance'},
+            {id = 'c_hermit'},
+            {id = 'c_devil'},
+            {id = 'c_familiar'},
+            {id = 'c_grim'},
+            {id = 'c_incantation'},
+            {id = 'c_talisman'},
+            {id = 'c_immolate'},
+            {id = 'j_delayed_grat'},
+            {id = 'j_business'},
+            {id = 'j_faceless'},
+            {id = 'j_todo_list'},
+            {id = 'j_vagabond'},
+            {id = 'j_cloud_9'},
+            {id = 'j_rocket'},
+            {id = 'j_midas_mask'},
+            {id = 'j_reserved_parking'},
+            {id = 'j_mail'},
+            {id = 'j_to_the_moon'},
+            {id = 'j_golden'},
+            {id = 'j_trading'},
+            {id = 'j_ticket'},
+            {id = 'j_certificate'},
+            {id = 'j_rough_gem'},
+            {id = 'j_matador'},
+            {id = 'j_satellite'},
+            {id = 'j_fortuno'},
+            {id = 'v_clearance_sale'},
+            {id = 'v_liquidation'},
+            {id = 'v_seed_money'},
+            {id = 'v_money_tree'},
+            {id = 'p_standard_normal_1', ids = {
+                'p_standard_normal_1','p_standard_normal_2','p_standard_normal_3','p_standard_normal_4','p_standard_jumbo_1','p_standard_jumbo_2','p_standard_mega_1','p_standard_mega_2',
+            }},
+        },
+        banned_tags = {
+            {id = 'tag_garbage'},
+            {id = 'tag_handy'},
+            {id = 'tag_skip'},
+            {id = 'tag_coupon'},
+            {id = 'tag_investment'},
+            {id = 'tag_economy'},
+        },
+    }
+})
 
-function SMODS.INIT.JankChallenges () 
-
-    local challenges = G.CHALLENGES
-	G.localization.misc.challenge_names["c_mod_JankChallenges_pawnshop"] = "Pawn Shop"
-	G.localization.misc.challenge_names["c_mod_JankChallenges_LoJ"] = "Legend of Jimbo"
-    
-    table.insert(G.CHALLENGES,#G.CHALLENGES+1,{
-        name = 'Pawn Shop',
-        id = 'c_mod_JankChallenges_pawnshop',
-        rules = {
-            custom = {
-                {id = 'no_reward_specific', value = 'Small'},
-                {id = 'no_reward_specific', value = 'Big'},
-                {id = 'no_extra_hand_money'},
-                {id = 'no_interest'},
-            },
-            modifiers = {
-                {id = 'dollars', value = 4},
-                {id = 'discards', value = 4},
-                {id = 'hands', value = 3},
-                {id = 'reroll_cost', value = 5},
-                {id = 'joker_slots', value = 5},
-                {id = 'consumable_slots', value = 2},
-                {id = 'hand_size', value = 8},
-            }
+SMODS.Challenge({
+    key = "LoJ",
+    rules = {
+        custom = {
+            {id = 'no_reward_specific', value = 'Boss'},
         },
-        jokers = {
-            {id = 'j_pawn', edition = 'negative', eternal = true},
-            {id = 'j_scrapper'},
-        },
-        consumeables = {
-        },
-        vouchers = {
-            {id = 'v_overstock_norm'},
-        },
-        deck = {
-            type = 'Challenge Deck',
-            cards = {{s='D',r='2',},{s='D',r='3',},{s='D',r='4',},{s='D',r='5',},{s='D',r='6',},{s='D',r='7',},{s='D',r='8',},{s='D',r='9',},{s='D',r='T',},{s='D',r='J',},{s='D',r='Q',},{s='D',r='K',},{s='D',r='A',g='Blue',},{s='C',r='2',},{s='C',r='3',},{s='C',r='4',},{s='C',r='5',},{s='C',r='6',},{s='C',r='7',},{s='C',r='8',},{s='C',r='9',},{s='C',r='T',},{s='C',r='J',},{s='C',r='Q',},{s='C',r='K',},{s='C',r='A',g='Blue',},{s='H',r='2',},{s='H',r='3',},{s='H',r='4',},{s='H',r='5',},{s='H',r='6',},{s='H',r='7',},{s='H',r='8',},{s='H',r='9',},{s='H',r='T',},{s='H',r='J',},{s='H',r='Q',},{s='H',r='K',},{s='H',r='A',g='Blue',},{s='S',r='2',},{s='S',r='3',},{s='S',r='4',},{s='S',r='5',},{s='S',r='6',},{s='S',r='7',},{s='S',r='8',},{s='S',r='9',},{s='S',r='T',},{s='S',r='J',},{s='S',r='Q',},{s='S',r='K',},{s='S',r='A',g='Blue',},}
-        },
-        restrictions = {
-            banned_cards = {
-                {id = 'c_temperance'},
-                {id = 'c_hermit'},
-                {id = 'c_devil'},
-                {id = 'c_familiar'},
-                {id = 'c_grim'},
-                {id = 'c_incantation'},
-                {id = 'c_talisman'},
-                {id = 'c_immolate'},
-                {id = 'j_delayed_grat'},
-                {id = 'j_business'},
-                {id = 'j_faceless'},
-                {id = 'j_todo_list'},
-                {id = 'j_vagabond'},
-                {id = 'j_cloud_9'},
-                {id = 'j_rocket'},
-                {id = 'j_midas_mask'},
-                {id = 'j_reserved_parking'},
-                {id = 'j_mail'},
-                {id = 'j_to_the_moon'},
-                {id = 'j_golden'},
-                {id = 'j_trading'},
-                {id = 'j_ticket'},
-                {id = 'j_certificate'},
-                {id = 'j_rough_gem'},
-                {id = 'j_matador'},
-                {id = 'j_satellite'},
-                {id = 'j_fortuno'},
-                {id = 'v_clearance_sale'},
-                {id = 'v_liquidation'},
-                {id = 'v_seed_money'},
-                {id = 'v_money_tree'},
-                {id = 'p_standard_normal_1', ids = {
-                    'p_standard_normal_1','p_standard_normal_2','p_standard_normal_3','p_standard_normal_4','p_standard_jumbo_1','p_standard_jumbo_2','p_standard_mega_1','p_standard_mega_2',
-                }},
-            },
-            banned_tags = {
-                {id = 'tag_garbage'},
-                {id = 'tag_handy'},
-                {id = 'tag_skip'},
-                {id = 'tag_coupon'},
-                {id = 'tag_investment'},
-                {id = 'tag_economy'},
-            },
-            banned_other = {
-            }
+        modifiers = {
+            {id = 'discards', value = 2},
+            {id = 'hands', value = 3},
+            {id = 'hand_size', value = 7},
         }
-    })
-
-    table.insert(G.CHALLENGES,#G.CHALLENGES+1,{
-        name = 'Legend of Jimbo',
-        id = 'c_mod_JankChallenges_LoJ',
-        rules = {
-            custom = {
-                {id = 'no_reward_specific', value = 'Boss'},
-            },
-            modifiers = {
-                {id = 'dollars', value = 4},
-                {id = 'discards', value = 2},
-                {id = 'hands', value = 3},
-                {id = 'reroll_cost', value = 5},
-                {id = 'joker_slots', value = 5},
-                {id = 'consumable_slots', value = 2},
-                {id = 'hand_size', value = 7},
-            }
+    },
+    jokers = {
+        {id = 'j_old_man', eternal = true}
+    },
+    deck = {
+        type = 'Challenge Deck'
+    },
+    restrictions = {
+        banned_cards = {
+            {id = 'c_ankh'},
+            {id = 'c_hex'},
         },
-        jokers = {
-            {id = 'j_old_man', eternal = true}
-        },
-        consumeables = {
-        },
-        vouchers = {
-        },
-        deck = {
-            type = 'Challenge Deck'
-        },
-        restrictions = {
-            banned_cards = {
-                {id = 'c_ankh'},
-                {id = 'c_hex'},
-            },
-            banned_tags = {
-            },
-            banned_other = {
-
-            }
-        }
-    })
-end
+    }
+})
 
 ----------------------------------------------
 ------------MOD CODE END----------------------
---
+++ localization/default.lua
@@ -0,0 +1,301 @@
+return {
+	descriptions = {
+		Joker = {
+			j_jank_pawn = {
+				name = "Pawn Joker",
+				text = {
+					"{C:green}#1# in #2#{} chance to",
+					"get {C:attention}$#3#{} when you",
+					"{C:attention}sell{} a card",
+				},
+			},
+			j_jank_expanded_art = {
+				name = "Expanded Art Joker",
+				text = {
+					"{C:attention}Enhanced Cards{} gives",
+					"{C:chips}+#1#{} Chips when scored",
+				},
+			},
+			j_jank_self_portrait = {
+				name = "Self Portrait",
+				text = {
+					"This Joker gains {X:mult,C:white}X0.1{} Mult",
+					"whenever you {C:attention}#1#{}.",
+					"Changes every blind.",
+					"{C:inactive}(Currently {X:mult,C:white}X#2#{}{C:inactive} Mult){}",
+				},
+			},
+			j_jank_mural_menace = {
+				name = "Mural Menace",
+				text = {
+					"Create a random {C:attention}Tag{}",
+					"when you skip a {C:attention}Blind{}",
+				},
+			},
+			j_jank_minimalist = {
+				name = "Minimalist Joker",
+				text = {
+					"{C:mult}+#1#{} Mult if played hand",
+					"contains no {C:attention}face cards{}",
+				},
+			},
+			j_jank_devoted = {
+				name = "Devoted Joker",
+				text = {
+					"When {C:attention}Boss Blind{} is selected",
+					"gain {X:mult,C:white}X#2#{} Mult, then",
+					"set your {C:attention}money{} to {C:attention}$0{}",
+					"{C:inactive}(Currently {X:mult,C:white} X#1# {C:inactive})",
+				},
+			},
+			j_jank_ternary_system = {
+				name = "Ternary System",
+				text = {
+					"Create a {C:planet}Planet{} card",
+					"if played hand contains {C:attention}3{}",
+					"cards and a {C:attention}Three of a Kind{}",
+					"{C:inactive}(Must have room)",
+				},
+			},
+			j_jank_stanczyk = {
+				name = "Stańczyk",
+				text = {
+					"Retrigger {C:attention}Enhanced Cards{}",
+					"that are played or",
+					"held in hand",
+				},
+			},
+			j_jank_cut_the_cheese = {
+				name = "Cut the Cheese",
+				text = {
+					"When {C:attention}Blind{} is selected",
+					"create a random {C:attention}Food Joker{}",
+					"{C:inactive}(Must have room){}",
+				},
+			},
+			j_jank_boredom_slayer = {
+				name = "Boredom Slayer",
+				text = {
+					"Reduce {C:attention}Blind{} requirements",
+					"by {C:attention}#1#%{} whenever you",
+					"play a hand",
+				},
+			},
+			j_jank_fortuno = {
+				name = "Fortuno",
+				text = {
+					"For each {C:attention}numbered card{} scored",
+					"in your first hand each round,",
+					"destroy it and gain {C:attention}$#1#{}",
+				},
+			},
+			j_jank_chalk_outline = {
+				name = "Chalk Outline",
+				text = {
+					"This Joker gains {C:mult}+6{} Mult",
+					"whenever you play",
+					"your {C:attention}final hand{}",
+					"{C:inactive}(Currently {C:mult}+#1#{C:inactive} Mult){}",
+				},
+			},
+			j_jank_memorable = {
+				name = "Memorable Joker",
+				text = {
+					"This Joker gains {X:mult,C:white} X#2# {} Mult",
+					"for each consecutive played hand",
+					"containing exactly {C:attention}3{} cards",
+					"{C:inactive}(Currently {X:mult,C:white}X#1#{}{C:inactive} Mult){}",
+				},
+			},
+			j_jank_scrapper = {
+				name = "Scrapper Joker",
+				text = {
+					"{C:green}#1# in #2#{} chance to create a",
+					"random {C:attention}Planet{} card when you",
+					"discard five {C:attention}numbered{} cards",
+				},
+			},
+			j_jank_lieutenant = {
+				name = "Lieutenant Joker",
+				text = {
+					"After you play a {C:attention}High Card{}",
+					"increase the {C:attention}rank{}",
+					"of each card played.",
+				},
+			},
+			j_jank_sunday_funnies = {
+				name = "Sunday Funnies",
+				text = {
+					"Create a random {C:planet}Planet{}",
+					"or {C:tarot}Tarot{} card per 2",
+					"{C:attention}rerolls{} in the shop",
+					"{C:inactive}({C:green}#1#{}{C:inactive} rerolls left){}",
+					"{C:inactive}(Must have room){}",
+				},
+			},
+			j_jank_box_of_stuff = {
+				name = "Box of Stuff",
+				text = {
+					"When {C:attention}Boss Blind{} is selected",
+					"create three free {C:attention}Standard Tags{},",
+					"then destroy this card",
+				},
+			},
+			j_jank_sentai = {
+				name = "Sentai Joker",
+				text = {
+					"Gains {C:mult}+#2#{} Mult per",
+					"{C:attention}Planet{} card used, resets",
+					"when {C:attention}Boss Blind{} is defeated",
+					"{C:inactive}(Currently {C:mult}+#1#{C:inactive} Mult)",
+				},
+			},
+			j_jank_old_man = {
+				name = "Old Man Joker",
+				text = {
+					"When {C:attention}Boss Blind{} is selected",
+					"create a free {C:attention}Ethereal Tag{}",
+				},
+			},
+			j_jank_cardslinger = {
+				name = "Cardslinger",
+				text = {
+					"{C:chips}+#1#{} Chips for each",
+					"time a card scored",
+					"this hand",
+				},
+			},
+			j_jank_shady_dealer = {
+				name = "Shady Dealer",
+				text = {
+					"Sell this card to create",
+					"a free {C:attention}Negative Tag{}",
+				},
+			},
+			j_jank_feste = {
+				name = "Feste",
+				text = {
+					"Upgrade the first hand",
+					"you play each {C:attention}Boss Blind{}",
+					"by {C:attention}#1#{} levels",
+				},
+			},
+			j_jank_chicken_scratch = {
+				name = "Chicken Scratch",
+				text = {
+					"This Joker gains {C:chips}+#2#{} Chips",
+					"if scoring hand contains",
+					"an {C:attention}8{}, {C:attention}7{}, or {C:attention}3{}",
+					"{C:inactive}(Currently {C:chips}+#1#{C:inactive} Chips){}",
+				},
+			},
+			j_jank_devilish = {
+				name = "Devilish Joker",
+				text = {
+					"{X:mult,C:white} X#1# {} Mult if played",
+					"hand contains only {C:attention}6s{}",
+					"or {C:attention}Gold Cards{}",
+				},
+			},
+			j_jank_pitiful = {
+				name = "Pitiful Joker",
+				text = {
+					"{C:mult}+#1#{} Mult if",
+					"played hand is a",
+					"{C:attention}High Card{} or {C:attention}Pair{}",
+				},
+			},
+			j_jank_tapestry = {
+				name = "Tapestry Joker",
+				text = {
+					"This Joker gains {C:mult}+#2#{} Mult for",
+					"each unique consumable used",
+					"{C:inactive}(Currently {C:mult}+#1#{}{C:inactive} Mult){}",
+				},
+			},
+			j_jank_wanted_poster = {
+				name = "Wanted Poster",
+				text = {
+					"Gain {C:attention}$#1#{} when you",
+					"beat a {C:attention}Blind{} first hand.",
+					"Lose {C:attention}$#2#{} when you play",
+					"a hand past your first",
+				},
+			},
+			j_jank_midnight_crew = {
+				name = "Midnight Crew",
+				text = {
+					"This Joker gains {X:mult,C:white} X0.5 {} Mult",
+					"if scoring hand contains a",
+					"{C:attention}Flush of{} {V:1}#1#{}. Changes",
+					"suit after every blind",
+					"{C:inactive}(Currently {X:mult,C:white}X#2#{C:inactive})",
+				},
+			},
+			j_jank_makeshift = {
+				name = "Makeshift Joker",
+				text = {
+					"This Joker gains {C:mult}+#2#{} Mult",
+					"per {C:attention}card{} sold",
+					"{C:inactive}(Currently {C:mult}+#1#{C:inactive} Mult)",
+				},
+			},
+			j_jank_sir = {
+				name = "Sir Joker",
+				text = {
+					"{X:mult,C:white} X#1# {} Mult during",
+					"{C:attention}Boss Blinds{} or",
+					"your {C:attention}final hand{}",
+				},
+			},
+			j_jank_suspicious_vase = {
+				name = "Suspicious Vase",
+				text = {
+					"All {C:attention}2s{}, {C:attention}3s{} and {C:attention}4s{}",
+					"become {C:attention}Glass Cards{} and",
+					"give {X:mult,C:white} X#1# {} Mult when played",
+				},
+			},
+			j_jank_impractical = {
+				name = "Impractical Joker",
+				text = {
+					"{X:mult,C:white} X#2# {} Mult if {C:attention}poker hand{}",
+					"is a {C:attention}#1#{},",
+					"poker hand changes on",
+					"every matching hand",
+				},
+			},
+			j_jank_highlander = {
+				name = "Highlander Joker",
+				text = {
+					"Every scoring card permanently",
+					"gains {C:chips}+#1#{} Chips when scored if",
+					"your hand is a {C:attention}High Card{}.",
+				},
+			},
+			j_jank_jevil = {
+				name = "Jevil",
+                text = {
+                    "{C:attention}Straight Flushes{} give",
+                    "{C:chips}+1{} Chips when scored"
+                }
+			}
+		},
+	},
+	misc = {
+		challenge_names = {
+			c_jankch_pawnshop = "Pawn Shop",
+			c_jankch_LoJ = "Legend of Jimbo",
+		},
+		dictionary = {
+			self_portrait_1 = "use a Tarot card",
+            self_portrait_2 = "use a Planet card",
+            self_portrait_3 = "destroy a card",
+            self_portrait_4 = "play a hand with 3 or fewer cards",
+            self_portrait_5 = "discard 3 or more face cards",
+            self_portrait_6 = "play a High Card",
+            self_portrait_7 = "play a Straight",
+            self_portrait_8 = "play a Flush",
+		}
+	}
+}
